83d609bd38dfa635f4f0cbdb72c90f54fb2d4de2
Cristian E. Ruiz Aguilar <cristianruiz1195@gmail.com>
2023-01-20 22:29:24 -0600

    Se crea la parte api del proyecto

---
+++ anyways-app-api/src/main/java/com/mexcrisoft/anyways/App.java
@@ -0,0 +1,13 @@
+package com.mexcrisoft.anyways;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
